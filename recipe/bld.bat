1f50921ca4aab69def188b56dddaf621db2f85e0
Charles Bousseau <cbousseau@anaconda.com>
2024-07-24 17:07:25 -0400

    unvendor libprotobuf libabseil

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,3 @@
 set GRPC_PYTHON_BUILD_WITH_CYTHON=1
+rmdir .\third_party\ /s /q
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export GRPC_PYTHON_BUILD_WITH_CYTHON=1
+rm -rf ./third_party/
 ${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
 
