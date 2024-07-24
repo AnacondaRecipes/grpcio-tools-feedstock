bfa52155f64382ddb66ae3fb2b0ff8c8f903826b
Charles Bousseau <cbousseau@anaconda.com>
2024-07-24 18:17:43 -0400

    fix win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
 set GRPC_PYTHON_BUILD_WITH_CYTHON=1
-rmdir .\third_party\ /s /q
+rem uncomment when unvendoring is fixed.
+rem rmdir .\third_party\ /s /q
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
